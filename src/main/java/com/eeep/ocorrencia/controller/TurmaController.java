96fcc3b2b5d79e73383697bf3f05d43c896f40af
Manuelle <manuellebrasil06@gmail.com>
2026-05-18 16:43:51 -0300

    equipe-1

---
+++ src/main/java/com/eeep/ocorrencia/controller/TurmaController.java
@@ -0,0 +1,30 @@
+package com.eeep.ocorrencia.controller;
+
+import com.eeep.ocorrencia.model.Turma;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TurmaController {
+
+    private List<Turma> turmas = new ArrayList<>();
+
+    @GetMapping("/")
+    public String abrirFormulario(Model model) {
+        model.addAttribute("listaturmas", turmas);
+        model.addAttribute("turma", new Turma());
+        return "cadastro";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarTurma(@ModelAttribute Turma turma) {
+        turmas.add(turma);
+        return "redirect:/";
+    }
+}
